fddde8a821d3cf6d06af6498d3a33ee2e79edb04
phiresky <phireskyde+git@gmail.com>
2021-11-26 15:16:34 +0100

    update dependencies

--- migrations/20211126070357_resetcache.sql
+++ migrations/20211126070357_resetcache.sql
@@ -1,1 +1,7 @@
 delete from extracted.extracted_current;
+
+create view extracted_chunks_view as 
+    select e.rowid as rowid, timechunk, tags.text as tag, tag_values.text as value, duration_ms
+        from extracted_chunks e
+        join tags on tags.id = e.tag
+        join tag_values on tag_values.id = e.value;
