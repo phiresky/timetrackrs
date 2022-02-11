1cadb268d43188ac7fd6cabc981f96b1edce5ee9
Selyatin Ismet <selyatinismet@gmail.com>
2022-02-11 22:33:51 +0000

    Use Accessibility API to get the Window Title

--- src/capture/macos/appkit.rs
+++ src/capture/macos/appkit.rs
@@ -1,14 +1,25 @@
 use super::types::*;
 use crate::prelude::*;
 
+use accessibility_sys::{
+    kAXErrorSuccess, kAXFocusedWindowAttribute, kAXTitleAttribute, kAXTrustedCheckOptionPrompt,
+    kAXWindowsAttribute, AXError, AXIsProcessTrustedWithOptions, AXUIElementCopyAttributeValue,
+    AXUIElementCreateApplication, AXUIElementRef,
+};
 use core_foundation::{
-    array::CFArray,
-    base::{FromVoid, ItemRef},
-    dictionary::CFDictionary,
+    array::{CFArray, CFArrayRef},
+    base::{CFRelease, FromVoid, ItemRef, TCFType, ToVoid},
+    boolean::CFBoolean,
+    dictionary::{CFDictionary, CFMutableDictionary},
     number::CFNumber,
     string::{kCFStringEncodingUTF8, CFString, CFStringGetCStringPtr, CFStringRef},
 };
-use core_graphics::window::{kCGNullWindowID, kCGWindowListOptionAll, CGWindowListCopyWindowInfo};
+use core_graphics::{
+    window::{
+        kCGNullWindowID, kCGWindowListOptionAll, kCGWindowListOptionOnScreenOnly, CGWindowID,
+        CGWindowListCopyWindowInfo,
+    },
+};
 use objc::{class, msg_send, runtime::Object, sel, sel_impl};
 use rustc_hash::FxHashMap;
 use std::{
@@ -17,25 +28,203 @@ use std::{
 };
 use sysinfo::{Pid, PidExt, ProcessExt, System, SystemExt};
 
+extern "C" {
+    fn _AXUIElementGetWindow(_: AXUIElementRef, _: *mut CGWindowID) -> AXError;
+}
+
 pub struct MacOSCapturer {
     os_info: util::OsInfo,
+    // Using Arc<String>, so that
+    // we don't clone the string when passing it to the MacOSWindow
+    // struct.
+    window_titles: FxHashMap<i32, Arc<String>>,
+    accessibility_permission: bool,
 }
 
 impl MacOSCapturer {
     pub fn init() -> MacOSCapturer {
+        let accessibility_permission = unsafe { check_accessibility_permission() };
         MacOSCapturer {
             os_info: util::get_os_info(),
+            window_titles: FxHashMap::default(),
+            accessibility_permission,
         }
+    }
+
+    /// Gets all currently running apps that may have UIs and are visible in the dock.
+    /// Reference: https://developer.apple.com/documentation/appkit/nsapplicationactivationpolicy?language=objc
+    pub fn get_windows(&mut self) -> (Option<Vec<usize>>, Vec<MacOSWindow>) {
+        let MacOSCapturer {
+            accessibility_permission,
+            ..
+        } = *self;
+
+        let mut on_screen_windows: Vec<usize> = vec![];
+
+        let mut windows: Vec<MacOSWindow> = vec![];
+
+        let mut process_data_map: FxHashMap<i32, Arc<MacOSProcessData>> = FxHashMap::default();
+
+        let mut ax_elements_map: FxHashMap<i32, ItemRef<CFArray<*const c_void>>> =
+            FxHashMap::default();
+
+        let mut system = System::new();
+
+        unsafe {
+            let shared_workspace: *mut Object = msg_send![class!(NSWorkspace), sharedWorkspace];
+
+            let running_applications: *mut Object =
+                msg_send![shared_workspace, runningApplications];
+
+            let count: usize = msg_send![running_applications, count];
+
+            for i in 0..count {
+                let ns_running_application: *mut Object =
+                    msg_send![running_applications, objectAtIndex: i];
+
+                if ns_running_application.is_null() {
+                    continue;
+                }
+
+                let pid: i32 = msg_send![ns_running_application, processIdentifier];
+
+                if pid == -1 {
+                    continue;
+                }
+
+                let activation_policy: isize = msg_send![ns_running_application, activationPolicy];
+
+                // Reference: https://developer.apple.com/documentation/appkit/nsapplicationactivationpolicy/
+                if activation_policy != 0 {
+                    continue;
+                }
+
+                let pid = Pid::from_u32(pid as u32);
+                
+                system.refresh_process(pid);
+
+                let procinfo = match system.process(pid) {
+                    Some(procinfo) => procinfo,
+                    None => continue,
+                };
+
+                let parent = match procinfo.parent() {
+                    Some(parent) => parent.as_u32() as i32,
+                    None => continue,
+                };
+
+                // If the process wasn't launched by launchd
+                // it means that it's not the parent process
+                // for example it might be some chrome rendering
+                // process, but not the main chrome process
+                if parent != 1 {
+                    continue;
+                }
+
+                let process_data = MacOSProcessData {
+                    name: procinfo.name().to_string(),
+                    exe: procinfo.exe().to_string_lossy().to_string(), // tbh i don't care if your executables have filenames that are not unicode
+                    status: procinfo.status().to_string().to_string(),
+                };
+
+                process_data_map.insert(procinfo.pid().as_u32() as i32, Arc::new(process_data));
+            }
+
+            log::debug!("AXElementMap: {:?}", ax_elements_map);
+
+            release(running_applications);
+
+            let frontmost_application: *mut Object =
+                msg_send![shared_workspace, frontmostApplication];
+
+            let frontmost_application_pid: i32 =
+                msg_send![frontmost_application, processIdentifier];
+
+            let cf_array: ItemRef<CFArray<CFDictionary<CFStringRef, *const c_void>>> =
+                CFArray::from_void(CGWindowListCopyWindowInfo(
+                    kCGWindowListOptionOnScreenOnly,
+                    kCGNullWindowID,
+                ) as *const _);
+
+            for window in cf_array.iter() {
+                let (keys, values) = window.get_keys_and_values();
+
+                let mut macos_window = MacOSWindow::default();
+
+                let mut pid: Option<i32> = None;
+
+                for i in 0..keys.len() {
+                    let key = CFStringGetCStringPtr(keys[i] as _, kCFStringEncodingUTF8);
+
+                    let key = CStr::from_ptr(key).to_str().unwrap();
+
+                    match key {
+                        "kCGWindowNumber" => {
+                            macos_window.window_id =
+                                CFNumber::from_void(values[i]).to_i32().unwrap();
+                        }
+                        "kCGWindowOwnerPID" => {
+                            pid = CFNumber::from_void(values[i]).to_i32();
+                        }
+                        _ => (),
+                    };
+                }
+
+                if let Some(pid) = pid {
+                    if let Some(process) = process_data_map.get(&pid) {
+                        macos_window.process = process.clone();
+
+                        if pid == frontmost_application_pid {
+                            if accessibility_permission {
+                                let app_ref = AXUIElementCreateApplication(pid);
+
+                                let mut ax_element_ref: *const c_void = std::ptr::null();
+
+                                if AXUIElementCopyAttributeValue(
+                                    app_ref,
+                                    CFString::from_static_string(kAXFocusedWindowAttribute)
+                                        .as_concrete_TypeRef(),
+                                    &mut ax_element_ref,
+                                ) == kAXErrorSuccess
+                                {
+                                    let mut cf_string: *const c_void = std::ptr::null();
+
+                                    if AXUIElementCopyAttributeValue(
+                                        ax_element_ref as *mut _,
+                                        CFString::from_static_string(kAXTitleAttribute)
+                                            .as_concrete_TypeRef(),
+                                        &mut cf_string,
+                                    ) == kAXErrorSuccess
+                                    {
+                                        let string = CFString::from_void(cf_string).to_string();
+
+                                        macos_window.title = Some(string);
+
+                                        CFRelease(cf_string);
+                                    }
+                                    CFRelease(ax_element_ref);
+                                }
+                                CFRelease(app_ref as *const _);
+                            }
+                            on_screen_windows.push(windows.len());
+                            windows.push(macos_window);
+                        }
+                    }
+                }
+            }
+        }
+        let on_screen_windows = if on_screen_windows.is_empty() {None} else {Some(on_screen_windows)};
+        (on_screen_windows, windows)
     }
 }
 
 impl Capturer for MacOSCapturer {
     fn capture(&mut self) -> anyhow::Result<EventData> {
-        let (focused_window, windows) = get_windows();
+        let (on_screen_windows, windows) = self.get_windows();
 
         Ok(EventData::macos_v1(MacOSEventData {
             os_info: self.os_info.clone(),
-            focused_window,
+            on_screen_windows,
             windows,
             duration_since_user_input: user_idle::UserIdle::get_time()
                 .map(|e| e.duration())
@@ -47,6 +236,21 @@ impl Capturer for MacOSCapturer {
                 }),
         }))
     }
+}
+
+/// Checks the Accessibility permission, if not available prompts the user for it.
+unsafe fn check_accessibility_permission() -> bool {
+    let mut dict: CFMutableDictionary<CFString, CFNumber> = CFMutableDictionary::new();
+
+    dict.add(
+        &CFString::from_void(kAXTrustedCheckOptionPrompt as *const c_void).to_owned(),
+        &1i64.into(),
+    );
+
+    let app_has_permissions =
+        AXIsProcessTrustedWithOptions(dict.into_untyped().to_void() as *const _);
+
+    app_has_permissions
 }
 
 /// Frees any Objects
@@ -82,165 +286,4 @@ unsafe fn ns_string_to_string(ns_string: *mut Object) -> Option<String> {
     libc::free(char_ptr);
 
     Some(string)
-}
-
-/// Gets all currently running apps that may have UIs and are visible in the dock.
-/// Reference: https://developer.apple.com/documentation/appkit/nsapplicationactivationpolicy?language=objc
-pub fn get_windows() -> (Option<i32>, Vec<MacOSWindow>) {
-    let mut focused_window_id: Option<i32> = None;
-
-    let mut windows: Vec<MacOSWindow> = vec![];
-
-    let mut process_data_map: FxHashMap<i32, Arc<MacOSProcessData>> = FxHashMap::default();
-
-    let mut system = System::new();
-
-    unsafe {
-        let shared_workspace: *mut Object = msg_send![class!(NSWorkspace), sharedWorkspace];
-
-        let running_applications: *mut Object = msg_send![shared_workspace, runningApplications];
-
-        let count: usize = msg_send![running_applications, count];
-
-        for i in 0..count {
-            let ns_running_application: *mut Object =
-                msg_send![running_applications, objectAtIndex: i];
-
-            if ns_running_application.is_null() {
-                continue;
-            }
-
-            let pid: i32 = msg_send![ns_running_application, processIdentifier];
-
-            if pid == -1 {
-                continue;
-            }
-
-            let activation_policy: isize = msg_send![ns_running_application, activationPolicy];
-
-            // Reference: https://developer.apple.com/documentation/appkit/nsapplicationactivationpolicy/
-            if activation_policy != 0 {
-                continue;
-            }
-
-            let pid = Pid::from_u32(pid as u32);
-
-            system.refresh_process(pid);
-
-            let procinfo = match system.process(pid) {
-                Some(procinfo) => procinfo,
-                None => continue,
-            };
-
-            let parent = match procinfo.parent() {
-                Some(parent) => parent.as_u32() as i32,
-                None => continue,
-            };
-
-            // If the process wasn't launched by launchd
-            // it means that it's not the parent process
-            // for example it might be some chrome rendering
-            // process, but not the main chrome process
-            if parent != 1 {
-                continue;
-            }
-
-            let process_data = MacOSProcessData {
-                pid: procinfo.pid().as_u32() as i32,
-                name: procinfo.name().to_string(),
-                cmd: procinfo.cmd().to_vec(),
-                exe: procinfo.exe().to_string_lossy().to_string(), // tbh i don't care if your executables have filenames that are not unicode
-                cwd: procinfo.cwd().to_string_lossy().to_string(),
-                memory_kB: procinfo.memory() as i64,
-                status: procinfo.status().to_string().to_string(),
-                start_time: util::unix_epoch_millis_to_date((procinfo.start_time() as i64) * 1000),
-                cpu_usage: Some(procinfo.cpu_usage()),
-                bundle: get_bundle_url(ns_running_application),
-                parent: Some(parent),
-            };
-
-            process_data_map.insert(process_data.pid, Arc::new(process_data));
-        }
-
-        release(running_applications);
-
-        let frontmost_application: *mut Object = msg_send![shared_workspace, frontmostApplication];
-
-        let frontmost_application_pid: i32 = msg_send![frontmost_application, processIdentifier];
-
-        release(frontmost_application);
-        release(shared_workspace);
-
-        let cf_array: ItemRef<CFArray<CFDictionary<CFStringRef, *const c_void>>> =
-            CFArray::from_void(
-                CGWindowListCopyWindowInfo(kCGWindowListOptionAll, kCGNullWindowID) as *const _,
-            );
-
-        for window in cf_array.iter() {
-            let (keys, values) = window.get_keys_and_values();
-
-            let mut macos_window = MacOSWindow::default();
-
-            let mut pid: Option<i32> = None;
-
-            for i in 0..keys.len() {
-                let key = CFStringGetCStringPtr(keys[i] as _, kCFStringEncodingUTF8);
-
-                let key = CStr::from_ptr(key).to_str().unwrap();
-
-                match key {
-                    // Will most likely be NULL, because few applications set the
-                    // Quartz window name.
-                    // Reference: https://developer.apple.com/documentation/coregraphics/kcgwindowname?language=objc
-                    "kCGWindowName" => {
-                        macos_window.title = Some(CFString::from_void(values[i]).to_string());
-                    }
-                    "kCGWindowNumber" => {
-                        macos_window.window_id = CFNumber::from_void(values[i]).to_i32().unwrap();
-                    }
-                    "kCGWindowOwnerPID" => {
-                        pid = CFNumber::from_void(values[i]).to_i32();
-                    }
-                    _ => (),
-                };
-            }
-
-            if let Some(pid) = pid {
-                if let Some(process) = process_data_map.get(&pid) {
-                    macos_window.process = Some(process.clone());
-                    if process.pid == frontmost_application_pid {
-                        focused_window_id = Some(macos_window.window_id);
-                    }
-                }
-            }
-
-            if macos_window.title.is_none() && macos_window.process.is_none() {
-                continue;
-            }
-
-            windows.push(macos_window);
-        }
-    }
-    (focused_window_id, windows)
-}
-
-/// Gets **bundleURL** of [NSRunningApplication](https://developer.apple.com/documentation/appkit/nsrunningapplication?language=objc).
-unsafe fn get_bundle_url(object: *mut Object) -> Option<String> {
-    let ns_url: *mut Object = msg_send![object, bundleURL];
-
-    if ns_url.is_null() {
-        return None;
-    }
-
-    let ns_string: *mut Object = msg_send![ns_url, absoluteString];
-
-    if ns_string.is_null() {
-        return None;
-    }
-
-    let bundle_url = ns_string_to_string(ns_string);
-
-    release(ns_url);
-
-    bundle_url
 }
--- src/capture/macos/types.rs
+++ src/capture/macos/types.rs
@@ -2,10 +2,7 @@
 use super::super::pc_common;
 use crate::prelude::*;
 
-use std::{
-    sync::Arc,
-    time::Duration
-};
+use std::{sync::Arc, time::Duration};
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct MacOSCaptureArgs {}
@@ -13,7 +10,7 @@ pub struct MacOSCaptureArgs {}
 #[cfg(target_os = "macos")]
 impl CapturerCreator for MacOSCaptureArgs {
     fn create_capturer(&self) -> anyhow::Result<Box<dyn Capturer>> {
-        Ok(Box::new(super::appkit::MacOSCapturer::init()))        
+        Ok(Box::new(super::appkit::MacOSCapturer::init()))
     }
 }
 
@@ -29,40 +26,41 @@ pub struct MacOSEventData {
     #[serde(default)]
     pub os_info: util::OsInfo,
     pub duration_since_user_input: Duration,
-    pub focused_window: Option<i32>,
+    pub on_screen_windows: Option<Vec<usize>>,
     pub windows: Vec<MacOSWindow>,
 }
 
 impl ExtractInfo for MacOSEventData {
     fn extract_info(&self) -> Option<Tags> {
-        let mut tags = Tags::new();
-        
+        if self.on_screen_windows.is_none() {
+            return None;
+        }
+
         if pc_common::is_idle(self.duration_since_user_input) {
             return None;
         }
+
+        let mut tags = Tags::new();
         
         self.os_info.to_partial_general_software(&mut tags);
 
-        if let Some(focused_window) = self.focused_window {
-            if let Some(window) = self.windows.iter().find(|w| w.window_id == focused_window) {
-                if let Some(process) = &window.process {
-                
-                let cls = Some((process.name.clone(), "".to_owned()));
-                
-                let window_title = match window.title {
-                    Some(ref string) => string.as_str(),
-                    None => "Unknown"
-                };
-                
-                tags.extend(pc_common::match_software(
-                    window_title,
-                    &cls,
-                    Some(&process.exe),
-                    None,
-                    Some(&process.cmd)
-                ));
-                }
-            }
+        for i in self.on_screen_windows.as_ref().unwrap() {
+            let window = &self.windows[*i];
+            log::debug!("Yep WinCock {:?}", window);
+            let cls = Some((window.process.name.clone(), "".to_owned()));
+
+            let window_title = match window.title {
+                Some(ref string) => string.as_str(),
+                None => "Unknown",
+            };
+
+            tags.extend(pc_common::match_software(
+                window_title,
+                &cls,
+                Some(&window.process.exe),
+                None,
+                None
+            ));
         }
 
         Some(tags)
@@ -73,20 +71,12 @@ impl ExtractInfo for MacOSEventData {
 pub struct MacOSWindow {
     pub window_id: i32,
     pub title: Option<String>,
-    pub process: Option<Arc<MacOSProcessData>>,
+    pub process: Arc<MacOSProcessData>,
 }
 
-#[derive(Debug, Serialize, Deserialize, TypeScriptify, Clone)]
+#[derive(Debug, Default, Serialize, Deserialize, TypeScriptify, Clone)]
 pub struct MacOSProcessData {
-    pub pid: i32,
     pub name: String,
-    pub bundle: Option<String>,
-    pub cmd: Vec<String>,
     pub exe: String,
-    pub cwd: String,
-    pub memory_kB: i64,
-    pub parent: Option<i32>,
     pub status: String,
-    pub start_time: DateTime<Utc>,
-    pub cpu_usage: Option<f32>, // can be NaN -> null
 }
