12730cba3e1fa73fbd691c2b17e3a4e719fc46b0
Haoran Lin <tdkgn@outlook.com>
2022-08-01 23:52:39 +0800

    fix some type error

--- frontend/src/components/Routes.tsx
+++ frontend/src/components/Routes.tsx
@@ -15,13 +15,14 @@ export const Routes: React.FC = observer(() => {
 	}
 })
 
-export const BrowserRouterProvider: React.FC = observer(({ children }) => {
-	const model = useLocalObservable(() => ({
-		routing: new Routing(router, new LocationService()),
-	}))
-	return (
-		<RouterContext.Provider value={model.routing}>
-			{children}
-		</RouterContext.Provider>
-	)
-})
+export const BrowserRouterProvider: React.FC<{ children: React.ReactNode }> =
+	observer(({ children }) => {
+		const model = useLocalObservable(() => ({
+			routing: new Routing(router, new LocationService()),
+		}))
+		return (
+			<RouterContext.Provider value={model.routing}>
+				{children}
+			</RouterContext.Provider>
+		)
+	})
